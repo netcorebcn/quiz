529632d986dec59070de7ccae0796c04ec4f5a59
paulopez78 <paulopez78@gmail.com>
2017-02-25 19:28:42 +0100

    fix default event store connection string

--- src/Quiz.EventSourcing/EventStoreConnectionFactory.cs
+++ src/Quiz.EventSourcing/EventStoreConnectionFactory.cs
@@ -7,7 +7,7 @@ namespace Quiz.EventSourcing
         public static IEventStoreConnection Create(string connectionString)
         {
             if (string.IsNullOrEmpty(connectionString))
-                connectionString = "tcp://user:password@localhost:1113";
+                connectionString = "tcp://admin:changeit@localhost:1113";
 
             var connection = EventStoreConnection.Create(new System.Uri(connectionString));
             connection.ConnectAsync().Wait();
