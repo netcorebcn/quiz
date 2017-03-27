cb0f6f8a26586b122a6595642cd0f97759794eeb
paulopez78 <paulopez78@gmail.com>
2017-03-27 23:57:47 +0200

    clean up

--- run.sh
+++ run.sh
@@ -1,10 +1,3 @@
-# docker rm -f quiz-voting-api
-# docker rm -f eventstore-node
-
-# docker run --name eventstore-node -d -p 2113:2113 -p 1113:1113 eventstore/eventstore
-# docker run --name quiz-voting-api -d -p 5000:80 quiz-voting
-
-
 ./build.sh
 docker rm -f $(docker ps -qa)
 docker-compose -f ./docker/swarm/docker-compose.yml up
