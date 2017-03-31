330013df59581f6faa58312c70dec637d2dfdc1d
paulopez78 <paulopez78@gmail.com>
2017-03-31 13:53:22 +0200

    ci bash script using docker

---
+++ src/awesome-ci/run.sh
@@ -0,0 +1,15 @@
+# Parameters:
+REPO=$1
+SHA=$2
+
+echo $REPO
+echo $SHA
+
+docker build -t quiz-"$SHA"-ci https://github.com/$REPO#$SHA -f ./docker/ci/Dockerfile.ci
+docker create --name quiz-"$SHA"-build quiz-"$SHA"-ci echo > null
+docker cp quiz-"$SHA"-build:/quizapp ./build-"$SHA"
+
+pushd build-"$SHA"
+./build.sh
+rm -rf build/"$SHA"
+popd
