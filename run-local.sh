aaec17695de1094529668d2dcd46edaa6b94b6e5
paulopez78 <paulopez78@gmail.com>
2017-04-01 12:25:04 +0200

    remove local donet cli runner

--- run-local.sh
+++
@@ -1,7 +0,0 @@
-docker rm -f eventstore-node
-docker run --name eventstore-node -d -p 2113:2113 -p 1113:1113 eventstore/eventstore
-
-dotnet restore
-dotnet run -p ./src/Quiz.EventSourcing.Setup/Quiz.EventSourcing.Setup.csproj &
-dotnet run -p ./src/Quiz.Results/Quiz.Results.csproj &
-dotnet run -p ./src/Quiz.Voting/Quiz.Voting.csproj &
