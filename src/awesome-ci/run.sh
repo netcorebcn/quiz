c39c0521d255c219fe1a841b960a61cbd30e733e
paulopez78 <paulopez78@gmail.com>
2017-04-01 12:14:19 +0200

    fix ci script for build and deploy to swarm

--- src/awesome-ci/run.sh
+++ src/awesome-ci/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPO=$1
-SHA=$2
+SHA=${2:-master}
 TOKEN=$3
 
 echo $REPO
@@ -12,14 +12,17 @@ docker build -t quiz-$SHA-ci https://github.com/$REPO#$SHA -f ./docker/ci/Docker
 docker create --name quiz-$SHA-build quiz-$SHA-ci echo > null
 docker cp quiz-$SHA-build:/quizapp ./build-$SHA
 
+# build docker images from the git checkout
 pushd build-$SHA
-
 ./build.sh $SHA
 
-if [ ! -z "$TOKEN" ] then
-    ./docker/swarm/deploy.sh $TOKEN $SHA
+# deploy stack to swarm using docker compose
+if [ ! -z "$TOKEN" ]; then
+    pushd docker/swarm
+    ./deploy.sh $TOKEN $SHA;
+    popd
 fi
 
 popd
 
-rm -rf build/$SHA
+rm -rf build-$SHA
