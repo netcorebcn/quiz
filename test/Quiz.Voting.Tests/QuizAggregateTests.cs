3f376e0c83d5dd3716f17a5ae91c3e93e47e6f50
Pau <pau.lopez.78@gmail.com>
2017-04-02 21:39:25 +0200

    not not

--- test/Quiz.Voting.Tests/QuizAggregateTests.cs
+++ test/Quiz.Voting.Tests/QuizAggregateTests.cs
@@ -78,7 +78,7 @@ namespace Quiz.Voting.Tests
             var startedEvent = quiz.GetPendingEvents().FirstOrDefault();
             var answeredEvent = quiz.GetPendingEvents().LastOrDefault();
 
-            Assert.NotNull(startedEvent);
+            Assert.Null(startedEvent);
             
             Assert.IsAssignableFrom(typeof(QuizStartedEvent), startedEvent);
             Assert.NotNull(answeredEvent);
