eb1d77e86e1a6c270f60482d8bfcce5ddfb5ebea
paulopez78 <paulopez78@gmail.com>
2017-04-04 11:48:55 +0200

    remove visualizer from provisioning of swarm

--- docker/swarm/local/provision.sh
+++ docker/swarm/local/provision.sh
@@ -39,10 +39,3 @@ done
 printf "\nLocal Swarm Cluster\n===================\n"
 
 docker node ls
-
-# echo swarm visualizer
-printf "\nLocal Swarm Visualizer\n===================\n"
-docker run -it -d --name swarm_visualizer \
-  -p 8000:8080 -e HOST=localhost \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  manomarks/visualizer:beta
