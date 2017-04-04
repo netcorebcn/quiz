feb008c8b3492ebf2c9dd8eb15aff05e205a37f2
paulopez78 <paulopez78@gmail.com>
2017-04-05 00:10:43 +0200

    split store , setup and app with hack sleep

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 ./build.sh
 docker rm -f $(docker ps -qa)
 docker-compose -f ./docker/docker-compose.eventstore.yml up -d
-sleep 10
+sleep 5
+docker-compose -f ./docker/docker-compose.eventstore.setup.yml up -d
+sleep 5
 docker-compose -f ./docker/docker-compose.yml up
