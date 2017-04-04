20f5743b94f2b7d2a357c72a75df22c224b2f5f0
paulopez78 <paulopez78@gmail.com>
2017-04-04 21:38:05 +0200

    use aspnet core results app instead of console

--- src/Quiz.Results/Program.cs
+++ src/Quiz.Results/Program.cs
@@ -1,45 +1,19 @@
-﻿using System;
-using System.Threading;
-using Microsoft.Extensions.Configuration;
-using Quiz.EventSourcing;
-using Quiz.Messages;
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace Quiz.Results
+namespace Quiz.Voting.Results
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var quitEvent = new ManualResetEvent(false);
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
 
-            Console.CancelKeyPress += (sender, eArgs) => {
-                quitEvent.Set();
-                eArgs.Cancel = true;
-                Console.WriteLine("Application is shutting down...");
-            };
-
-            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
-            var configuration = builder.Build();
-                        
-            var options = EventStoreOptions.Create(configuration);
-            var typeResolver = new EventTypeResolver(ReflectionHelper.MessagesAssembly);
-            var eventBus = EventStoreConnectionFactory.Create(options.ConnectionString);
-        
-            eventBus.Subscribe(
-                typeResolver, options.Subscription, 
-                msg => 
-                {
-                    if (msg is QuestionStatisticCreatedEvent questionAnswersStats)
-                        Console.WriteLine($@"
-                            Type:{questionAnswersStats.GetType().Name}, 
-                            OptionId:{questionAnswersStats.QuestionId}, 
-                            RightAnswersPercent: {questionAnswersStats.RightAnswersPercent}
-                            WrongAnswersPercent: {questionAnswersStats.WrongAnswersPercent}");                    
-                })
-            .Wait();
-
-            Console.Write("Application started. Press Ctrl+C to shut down.");
-            quitEvent.WaitOne();
-        }      
-     }
+            host.Run();
+        }
+    }
 }
