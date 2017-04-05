46cd79fba24d40d6d01e346ce880989f5987f062
paulopez78 <paulopez78@gmail.com>
2017-04-05 23:37:33 +0200

    call quiz api in App component

--- src/quiz-ui/src/App.js
+++ src/quiz-ui/src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:81/quiz/1')
+      .then(r => r.json())
+      .then(json => { 
+        console.log(json);
+        this.setState({ questions: json.questions });
+      });  
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +25,14 @@ class App extends Component {
           <h2>Welcome to Quiz</h2>
         </div>
         <p className="App-intro">
-
+            {this.state.questions.map(q =>
+              <p> 
+                  {q.description}
+                  <ul key={q.id}>    
+                    {q.options.map(o => <li key={o.id}>{o.description}</li>)}
+                  </ul>
+              </p>
+            )}
         </p>
       </div>
     );
