5bc24ad41f0a7081034c598059854dae5d71b8d0
paulopez78 <paulopez78@gmail.com>
2017-04-06 21:46:12 +0200

    first working harcoded ui

--- src/quiz-ui/src/App.js
+++ src/quiz-ui/src/App.js
@@ -5,17 +5,59 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      quizId: 0,
       questions: []
     };
+
+    this.startQuiz = this.startQuiz.bind(this);
+    this.voteQuestion = this.voteQuestion.bind(this);
   }
 
   componentDidMount() {
-    fetch('http://localhost:81/quiz/1')
+    var webSocket = new WebSocket('ws://localhost:82/ws');
+
+    webSocket.onopen = (e) => {
+      console.log(e);
+    };
+
+    webSocket.onmessage = (e) => {
+      console.log(e.data);
+      var questionStats = JSON.parse(e.data);
+
+      this.setState({ ...this.state,
+        questions: this.state.questions.map(
+          question => question.id === questionStats.questionId 
+            ? { ...question, ...questionStats  } 
+            : question) 
+      });
+    }
+  }
+
+  startQuiz(quizId) {
+    fetch(`http://localhost:81/quiz/${quizId}`, { method: 'PUT'})
+      .then(r => r.json())
+      .then(json => {
+        console.log(json);
+        this.setState({ ...json });
+      });  
+  }
+
+  voteQuestion(questionId, optionId) {
+    fetch(`http://localhost:81/quiz/${this.state.quizId}`, 
+      { 
+        method: 'POST', 
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({questionId, optionId})
+      })
       .then(r => r.json())
       .then(json => { 
         console.log(json);
         this.setState({ questions: json.questions });
-      });  
+      })
+      .catch(e => console.log(e));  
   }
 
   render() {
@@ -24,14 +66,23 @@ class App extends Component {
         <div className="App-header">
           <h2>Welcome to Quiz</h2>
         </div>
+            QuizId:{this.state.quizId}
             {this.state.questions.map(q =>
               <ul key={q.id}>    
-                {q.description}
+                {q.id.slice(0,7)}-{q.description} 
+                - Right {q.rightAnswersPercent || 0} 
+                - Wrong {q.wrongAnswersPercent || 0}
                 {q.options.map(o => 
-                  <li key={o.id}>{o.description}</li>
+                  <li key={o.id} onClick={() => this.voteQuestion(q.id,o.id)}>
+                    {o.id.slice(0,7)}-{o.description}
+                  </li>
                   )}
               </ul>
             )}
+
+            <button onClick={() => this.startQuiz(1)}>
+              Start Quiz
+            </button>
       </div>
     );
   }
--- src/quiz-ui/src/index.js
+++ src/quiz-ui/src/index.js
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-var webSocket = new WebSocket('ws://localhost:82/ws');
-webSocket.onopen = function (event) {
-  console.log(event);
-};
-
-webSocket.onmessage = function (event) {
-  console.log(event.data);
-}
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
