4fe2320d294b036a12488eb25f99cd79c1be4b0f
paulopez78 <paulopez78@gmail.com>
2017-04-08 14:00:58 +0200

    add setup services to docker compose

--- docker/swarm/start.sh
+++ docker/swarm/start.sh
@@ -2,15 +2,7 @@ CI_TOKEN=$1
 REPO=$2
 REGISTRY=$3
 
-docker service create --name registry --publish 5000:5000 registry:2
-docker service create \
-  --publish=9090:8080 \
-  --limit-cpu 0.5 \
-  --name=viz \
-  --env PORT=9090 \
-  --constraint=node.role==manager \
-  --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock \
-  manomarks/visualizer
+docker deploy --compose-file docker-compose.setup.yml stack
 
 #run CI/CD on master branch
 pushd ../../src/awesome-ci
