83fc91f47a816fb64b6acbeaea64d95cee60ca50
Pau <pau.lopez.78@gmail.com>
2017-04-11 02:47:33 +0200

    test

--- src/Quiz.Messages/QuizModel.cs
+++ src/Quiz.Messages/QuizModel.cs
@@ -59,8 +59,7 @@ namespace Quiz.Messages
                     new List<QuestionOption> {
                         new QuestionOption(Guid.NewGuid(), "AWS"),
                         new QuestionOption(Guid.NewGuid(), "Azure", true),
-                        new QuestionOption(Guid.NewGuid(), "GCE"),
-                        new QuestionOption(Guid.NewGuid(), "Digital Ocean")
+                        new QuestionOption(Guid.NewGuid(), "GCE")
                     }),
                 new Question(Guid.NewGuid(), "Which is the Docker native built-in orchestrator?", 
                     new List<QuestionOption> {
