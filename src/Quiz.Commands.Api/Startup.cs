1231f13d9af2b85d7d802e79d40d1aa2d1968620
paulopez78 <pau.lopez.78@gmail.com>
2018-02-15 13:02:55 +0100

    hardcode the message broker

--- src/Quiz.Commands.Api/Startup.cs
+++ src/Quiz.Commands.Api/Startup.cs
@@ -30,7 +30,8 @@ namespace Quiz.Api
                     .AllowCredentials());
             })
             .AddEventStore(Configuration)
-            .AddSingleton<IBus>(RabbitHutch.CreateBus(Configuration["messagebroker"] ?? "amqp://guest:guest@localhost:5672"))
+            // .AddSingleton<IBus>(RabbitHutch.CreateBus(Configuration["messagebroker"] ?? "amqp://guest:guest@localhost:5672"))
+            .AddSingleton<IBus>(RabbitHutch.CreateBus("amqp://guest:guest@messagebroker:5672"))
             .AddTransient<QuizAppService>()
             .AddMvc();
 
