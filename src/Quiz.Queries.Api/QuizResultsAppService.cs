7e476582a1b2203772bbdc09ba2a7540bf6c961a
paulopez78 <pau.lopez.78@gmail.com>
2018-02-20 17:58:42 +0100

    add trim when reading config env

--- src/Quiz.Queries.Api/QuizResultsAppService.cs
+++ src/Quiz.Queries.Api/QuizResultsAppService.cs
@@ -64,7 +64,7 @@ namespace Quiz.Results.Api
     {
         public static IServiceCollection AddQuizResultsApp(this IServiceCollection services, IConfiguration configuration) => services
             .AddEasyWebSockets()
-            .AddSingleton<IBus>(RabbitHutch.CreateBus(configuration["messagebroker"] ?? "amqp://guest:guest@localhost:5672"))
+            .AddSingleton<IBus>(RabbitHutch.CreateBus(configuration["messagebroker"].Trim() ?? "amqp://guest:guest@localhost:5672"))
             .AddSingleton<QuizResultsAppService>();
         
         public static IApplicationBuilder UseQuizResultsApp(this IApplicationBuilder app)
