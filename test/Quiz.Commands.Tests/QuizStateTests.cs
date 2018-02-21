8f1728d8c75a1ebc3469874a1ace7e3340de8a43
paulopez78 <pau.lopez.78@gmail.com>
2018-02-21 14:42:00 +0100

    break unit test

--- test/Quiz.Commands.Tests/QuizStateTests.cs
+++ test/Quiz.Commands.Tests/QuizStateTests.cs
@@ -9,13 +9,13 @@ namespace Quiz.Domain.Tests
         [Theory]
         [InlineData(typeof(QuizStartedEvent))]
         public void Given_QuizState_When_Empty_Then_CanRaiseEvent(Type eventType) => 
-            Assert.False(QuizState.Created.CanRaiseEvent(eventType));
+            Assert.True(QuizState.Created.CanRaiseEvent(eventType));
 
         [Theory]
         [InlineData(typeof(QuizClosedEvent))]
         [InlineData(typeof(QuizAnsweredEvent))]
         public void Given_QuizState_When_Empty_Then_CanNotRaiseEvent(Type eventType) => 
-            Assert.True(QuizState.Created.CanRaiseEvent(eventType));
+            Assert.False(QuizState.Created.CanRaiseEvent(eventType));
 
         [Theory]
         [InlineData(typeof(QuizClosedEvent))]
