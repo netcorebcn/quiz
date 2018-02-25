e271d60862ec205966db6f1820b6239bba81b009
paulopez78 <pau.lopez.78@gmail.com>
2018-02-25 19:27:40 +0100

    fix path to yml

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
+for env in ENVIRONMENT REGISTRY TAG 
+do
+    pattern+='s/${'$env'}/'${!env}'/g;'
+done
+
 for deploy in k8s/*.yml
 do
-    for env in ENVIRONMENT REGISTRY TAG 
-    do
-        pattern+='s/${'$env'}/'${!env}'/g;'
-    done
-
-    sed $pattern ./jenkins/deploy.yml | kubectl apply -f - 
+    sed $pattern $deploy | kubectl apply -f - 
 done
