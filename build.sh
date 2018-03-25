13ffe8f0fbf594f9aae319d5fccf8b5b33d8db7c
paulopez78 <pau.lopez.78@gmail.com>
2018-03-25 23:02:00 +0200

    add registry for login

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ docker-compose build
 
 if [ -n "$REGISTRY" ]; then
     if [ -n "$REGISTRY_USER" ]; then
-        docker login -u ${REGISTRY_USER} -p ${REGISTRY_PASS}
+        docker login ${REGISTRY} -u ${REGISTRY_USER} -p ${REGISTRY_PASS}
     fi
     docker-compose push
 fi
--- jenkins/build.sh
+++ jenkins/build.sh
@@ -3,10 +3,8 @@ set -e
 docker-compose build
 
 if [ -n "$REGISTRY" ]; then
-
     if [ -n "$REGISTRY_USER" ]; then
-        docker login -u ${REGISTRY_USER} -p ${REGISTRY_PASS}
+        docker login ${REGISTRY} -u ${REGISTRY_USER} -p ${REGISTRY_PASS}
     fi
-
     docker-compose push
 fi
