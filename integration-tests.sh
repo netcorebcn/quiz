ac802595abee6a93ccf22189f246eebc6c64870b
paulopez78 <pau.lopez.78@gmail.com>
2018-04-02 22:56:01 +0200

    use environment when integration tests

--- integration-tests.sh
+++ integration-tests.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-kubectl delete pod integration-tests
+
+namespace=${ENVIRONMENT}
+
+kubectl delete pod integration-tests --namespace=$namespace
 
 set -e
 kubectl run --attach integration-tests \
 --image=${REGISTRY}/quiz-cli:${TAG} \
 --restart=Never \
 --env="QUIZ_URL=quiz-commands" \
---env="ITERATIONS=5" 
+--env="ITERATIONS=5" \
+--namespace=$namespace
 
 set +e
-kubectl delete pod integration-tests
+kubectl delete pod integration-tests --namespace=$namespace
