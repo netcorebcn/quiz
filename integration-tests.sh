fe6af5cfb8501cf5358769f3954c861e0518bd01
paulopez78 <pau.lopez.78@gmail.com>
2018-06-10 23:23:31 +0200

    add integration test dockerfiles and helm test

--- integration-tests.sh
+++ integration-tests.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 set -e
 chart='quiz'
+chartPath='./deploy/'$chart
 deploy=$chart-${BRANCH} 
 
-helm init --client-only
+# cleanup previous execution
 helm delete $deploy --purge > /dev/null 2>&1 || true
 
 helm dep update ./deploy/$chart
 helm upgrade --install \
     $deploy \
+    $chartPath \
     --namespace $deploy \
     --set imageRegistry=${REGISTRY} \
     --set imageTag=${TAG} \
-    --set postgresql.postgresPassword= ${POSTGRES_PASSWORD} \
-    --set rabbitmq.password= ${RABBIT_PASSWORD} \
-    --set rabbitmq.ingress.host = $deploy-rabbit.quiz.io \
-    ./deploy/$chart \
+    --set postgresql.postgresPassword=${POSTGRES_PASSWORD} \
+    --set rabbitmq.rabbitmq.password=${RABBIT_PASSWORD} \
+    --set rabbitmq.ingress.host=$deploy-rabbit.quiz.io \
     --debug \
     --wait
 
-helm test $deploy --verbose
+helm test --cleanup $deploy
+
+# cleanup
 helm delete $deploy --purge
+kubectl delete ns $deploy
