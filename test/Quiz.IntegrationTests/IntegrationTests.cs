2e49f590c39b58874086944fdd930d557dcb78f7
paulopez78 <pau.lopez.78@gmail.com>
2018-07-01 17:53:21 +0200

    fix integrationt tests

--- test/Quiz.IntegrationTests/IntegrationTests.cs
+++ test/Quiz.IntegrationTests/IntegrationTests.cs
@@ -60,7 +60,7 @@ namespace Quiz.Api.Tests
             var result = queryService.Get();
 
             Assert.NotNull(result);
-            Assert.Equal(1000.0M, result.TotalCorrectAnswersPercent);
+            Assert.Equal(100.0M, result.TotalCorrectAnswersPercent);
             Assert.Equal(0.0M, result.TotalIncorrectAnswersPercent);
         }
 
@@ -86,8 +86,8 @@ namespace Quiz.Api.Tests
             var result = queryService.Get();
 
             Assert.NotNull(result);
-            Assert.Equal(500.0M, result.TotalCorrectAnswersPercent);
-            Assert.Equal(500.0M, result.TotalIncorrectAnswersPercent);
+            Assert.Equal(50.0M, result.TotalCorrectAnswersPercent);
+            Assert.Equal(50.0M, result.TotalIncorrectAnswersPercent);
             Assert.Equal(2, result.Questions.Count);
         }
 
